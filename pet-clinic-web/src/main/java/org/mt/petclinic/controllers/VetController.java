d867954d73f034ed9a3549bea546bc4607aa2b3f
fingerdust <mtspark@gmail.com>
2019-11-02 19:57:25 +0000

    Added list vets Closes #15

--- pet-clinic-web/src/main/java/org/mt/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/mt/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package org.mt.petclinic.controllers;
 
+import org.mt.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 	
-	@RequestMapping({"/vets", "/vets/index"})
-	public String listAllVets() {
+	private final VetService vetService; 
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"", "/", "/index"})
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll()); 
 		return "vets/index"; 
 	}
 }
